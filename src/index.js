b3c5062c8fb2c85e0015171c31ffc9efb32e42fe
Luis <luisalgar@hotmail.com>
2021-11-06 17:38:08 -0300

    Heade-Nav

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Components/Header/Header';
+import reportWebVitals from './reportWebVitals';
+
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+
+
+
+
+
+//ReactDOM.render(elemento de react, elemntod donde se va a renderirar del html)
+/* este es el elemento de react
+
+<React.StrictMode>
+    <App />
+  </React.StrictMode>,
+
+  document.getElementById('root') //este es el elemnto del html
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
